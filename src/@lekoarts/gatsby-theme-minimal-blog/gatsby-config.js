d59612145cf878b9e2d58936e81e957a12ae2ee7
gommit <gommit@gmail.com>
2021-09-13 16:53:30 +0200

    Update gatsby-config.js

--- src/@lekoarts/gatsby-theme-minimal-blog/gatsby-config.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/gatsby-config.js
@@ -1,21 +1,20 @@
-module.exports = (options) => ({
+module.exports = {
   siteMetadata: {
+    // Used for the title template on pages other than the index site
     siteTitle: `Gommit`,
-    siteTitleAlt: `Gommit`,
+    // Default title of the page
+    siteTitleAlt: `Gommit - Digital garden`,
+    // Can be used for e.g. JSONLD
     siteHeadline: `Gommit`,
+    // Will be used to generate absolute URLs for og:image etc.
     siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `My Digital Garden`,
+    // Used for SEO
+    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    // Will be set on the <html /> tag
     siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    // Twitter Handle
+    author: `@gommit`,
   },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog-core`,
-      options,
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-theme-ui`,
-  ],
-})
+};
